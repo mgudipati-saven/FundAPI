81ad03aaaada77e793e54f562343d89ac7691993
Murthy Gudipati <mgudipati@saventech.com>
2013-02-21 18:42:41 +0530

    new redis db layour for auto completion
    key => "MFTickersAutoComplete"
    val => sorted set of fund tickers with auto completion list (google
    suggest).
    
    key => "MFNamesAutoComplete"
    val => sorted set of fund names with auto completion list (google
    suggest).

--- src/gen-auto-complete.rb
+++ src/gen-auto-complete.rb
@@ -1,11 +1,9 @@
 =begin
-key => "fund.tickers.auto.complete"
+key => "MFTickersAutoComplete"
 val => sorted set of fund tickers with auto completion list (google suggest).
-e.g.=> "fund.tickers.auto.complete" => ['A', 'AA', 'AAA', 'AAAAX', ...]
 
-key => "fund.names.auto.complete"
+key => "MFNamesAutoComplete"
 val => sorted set of fund names with auto completion list (google suggest).
-e.g.=> "fund.names.auto.complete" => ['A', 'AC', ...]
 =end
 
 require 'redis'
@@ -15,25 +13,25 @@ $redisdb.select 0
 
 # create a sorted set of google suggestions for fund tickers
 puts "Generating google suggestion list for fund tickers..."
-$redisdb.zunionstore "all.tickers", ["fund.tickers", "etf.tickers"]
-arr = $redisdb.zrange "all.tickers", 0, -1
+$redisdb.zunionstore "AllTickers", ["MFTickers", "ETFTickers"]
+arr = $redisdb.zrange "AllTickers", 0, -1
 arr.each do |ticker|
   (1..(ticker.length)).each do |n|
     prefix = ticker[0...n]
-    $redisdb.zadd "fund.tickers.auto.complete", 0, prefix
+    $redisdb.zadd "MFTickersAutoComplete", 0, prefix
   end
-  $redisdb.zadd "fund.tickers.auto.complete", 0, "#{ticker}*"
+  $redisdb.zadd "MFTickersAutoComplete", 0, "#{ticker}*"
 end
 
 # create a sorted set of google suggestions for fund names
 puts "Generating google suggestion list for fund names..."
-arr = $redisdb.zrange "fund.names", 0, -1
+arr = $redisdb.zrange "MFNames", 0, -1
 arr.each do |name|
   (1..(name.length)).each do |n|
     prefix = name[0...n]
-    $redisdb.zadd "fund.names.auto.complete", 0, prefix
+    $redisdb.zadd "MFNamesAutoComplete", 0, prefix
   end
-  $redisdb.zadd "fund.names.auto.complete", 0, "#{name}*"
+  $redisdb.zadd "MFNamesAutoComplete", 0, "#{name}*"
 end
 
 =begin rdoc
