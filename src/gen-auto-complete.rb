e7ab989898b9eeac4b95d89daf60f5afa76b03bc
Murthy Gudipati <mgudipati@saventech.com>
2013-02-22 09:02:31 +0530

    Revert "new redis db layour for auto completion"
    This reverts commit 81ad03aaaada77e793e54f562343d89ac7691993.

--- src/gen-auto-complete.rb
+++ src/gen-auto-complete.rb
@@ -1,9 +1,11 @@
 =begin
-key => "MFTickersAutoComplete"
+key => "fund.tickers.auto.complete"
 val => sorted set of fund tickers with auto completion list (google suggest).
+e.g.=> "fund.tickers.auto.complete" => ['A', 'AA', 'AAA', 'AAAAX', ...]
 
-key => "MFNamesAutoComplete"
+key => "fund.names.auto.complete"
 val => sorted set of fund names with auto completion list (google suggest).
+e.g.=> "fund.names.auto.complete" => ['A', 'AC', ...]
 =end
 
 require 'redis'
@@ -13,25 +15,25 @@ $redisdb.select 0
 
 # create a sorted set of google suggestions for fund tickers
 puts "Generating google suggestion list for fund tickers..."
-$redisdb.zunionstore "AllTickers", ["MFTickers", "ETFTickers"]
-arr = $redisdb.zrange "AllTickers", 0, -1
+$redisdb.zunionstore "all.tickers", ["fund.tickers", "etf.tickers"]
+arr = $redisdb.zrange "all.tickers", 0, -1
 arr.each do |ticker|
   (1..(ticker.length)).each do |n|
     prefix = ticker[0...n]
-    $redisdb.zadd "MFTickersAutoComplete", 0, prefix
+    $redisdb.zadd "fund.tickers.auto.complete", 0, prefix
   end
-  $redisdb.zadd "MFTickersAutoComplete", 0, "#{ticker}*"
+  $redisdb.zadd "fund.tickers.auto.complete", 0, "#{ticker}*"
 end
 
 # create a sorted set of google suggestions for fund names
 puts "Generating google suggestion list for fund names..."
-arr = $redisdb.zrange "MFNames", 0, -1
+arr = $redisdb.zrange "fund.names", 0, -1
 arr.each do |name|
   (1..(name.length)).each do |n|
     prefix = name[0...n]
-    $redisdb.zadd "MFNamesAutoComplete", 0, prefix
+    $redisdb.zadd "fund.names.auto.complete", 0, prefix
   end
-  $redisdb.zadd "MFNamesAutoComplete", 0, "#{name}*"
+  $redisdb.zadd "fund.names.auto.complete", 0, "#{name}*"
 end
 
 =begin rdoc
